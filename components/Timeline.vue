53d80d7087abd924492b27bed7ba8e799fe1ab20
Ricardo Boss <r.boss@trenz.de>
2020-07-17 19:49:54 +0200

    Updated timeline design

--- components/Timeline.vue
+++ components/Timeline.vue
@@ -2,10 +2,10 @@
 	<ol class="timeline">
     <li v-for="(milestone, i) in milestones" :key="i"
         class="timeline-item" :class="i === 0 ? 'current' : ''">
-      <div class="timeline-timespan" v-html="timespan(milestone)"></div>
       <div class="timeline-line">
         <div class="timeline-dot"></div>
       </div>
+      <div class="timeline-timespan" v-html="timespan(milestone)"></div>
       <div class="timeline-content">
         <h5 v-if="milestone.hasOwnProperty('title')" class="timeline-title">{{ milestone.title }}</h5>
         <p v-if="milestone.hasOwnProperty('description')" class="timeline-description">
@@ -27,14 +27,22 @@
     methods: {
       timespan(milestone) {
 		    if (!milestone.hasOwnProperty('to'))
-          return `since ${milestone.from}`;
+          return `since ${this.fromText(milestone.from)}`;
 
 		    if (milestone.from === milestone.to)
-		      return milestone.from;
+		      return this.fromText(milestone.from);
 
-        return `<span class="d-none d-md-inline"><span class="text-nowrap">${milestone.from}</span> - </span><span class="text-nowrap">${milestone.to}</span>`;
+        return `<span class="d-none d-md-inline"><span class="text-nowrap">${this.fromText(milestone.from)}</span> - </span><span class="text-nowrap">${this.toText(milestone.to)}</span>`;
+      },
+
+      fromText(from) {
+        return this.$moment(from).format("MMMM YYYY");
+      },
+
+      toText(to) {
+        return this.$moment(to).format("MMMM YYYY");
       }
-    }
+    },
 	}
 </script>
 
