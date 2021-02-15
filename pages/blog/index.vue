28365dde66e12b57efb08172b32c422f031647c3
Ricardo <contact@ricardoboss.de>
2021-02-15 22:40:38 +0100

    New blog index design

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,21 +2,21 @@
   <section>
     <h1 class="display-3">Blog</h1>
 
-    <div class="list-group list-group-flush rounded ml-3">
+    <div class="blog-entry-list">
       <nuxt-link v-for="post in sortedPosts" :key="post.attributes.title"
-                 class="list-group-item list-group-item-action row align-items-center"
+                 class="d-flex flex-column py-3 p-2 border-bottom blog-entry"
                  :to="getPermalink(post)">
-        <p class="text-nowrap small text-muted col-12 col-md-4">
+        <h5 class="mb-0 entry-title">
+          <span class="text-muted mr-2 num-sign">&num;</span>{{ post.attributes.title }}
+        </h5>
+
+        <small class="text-nowrap text-muted">
           {{ $moment(post.attributes.timestamp).fromNow() }} on {{ $moment(post.attributes.timestamp).format('L') }}
-        </p>
+        </small>
 
-        <div class="col-12 col-md-8">
-          <h5>{{ post.attributes.title }}</h5>
-
-          <p>{{ post.attributes.summary }}</p>
-        </div>
+        <p class="m-3">{{ post.attributes.summary }}</p>
       </nuxt-link>
-      <div v-if="posts.length === 0" class="list-group-item">
+      <div v-if="posts.length === 0">
         No posts were found.
       </div>
     </div>
@@ -80,3 +80,32 @@
     }
 	}
 </script>
+
+<style scoped lang="scss">
+.blog-entry-list {
+  display: flex;
+  flex-direction: column;
+}
+
+.num-sign {
+    transition: all 0.2s;
+}
+
+.blog-entry {
+  color: inherit;
+  text-decoration: none;
+    transition: all 0.2s;
+
+  &:hover {
+    background: rgba(255,255,255,0.1);
+
+    .entry-title {
+      color: var(--bs-primary);
+
+      .num-sign {
+        color: inherit !important;
+      }
+    }
+  }
+}
+</style>
