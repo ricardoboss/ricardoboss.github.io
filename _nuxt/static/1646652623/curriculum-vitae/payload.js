0e3247e698205c2cceb974089859168d82135ea7
ricardoboss <ricardoboss@users.noreply.github.com>
2022-03-07 11:30:31 +0000

    deploy: c899016a3fd1aea33476a10e902413ed40fcd918

---
+++ _nuxt/static/1646652623/curriculum-vitae/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/curriculum-vitae", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1646652623/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646652623",layout:"default",error:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2271d87d"}}}}(null,"\u002F"));
