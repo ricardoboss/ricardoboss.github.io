19f8dcbaa1c7876ca2250b6e89e074dc6ce3acbc
ricardoboss <ricardoboss@users.noreply.github.com>
2022-04-10 07:59:27 +0000

    deploy: 3669ff37a2d5d41bfd048a3836f1f6a7c70ed9dc

---
+++ _nuxt/static/1649577559/blog/2020-09-27-long-time-no-content/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649577559",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2020-09-27-long-time-no-content",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c56af030"}}}}(null));
