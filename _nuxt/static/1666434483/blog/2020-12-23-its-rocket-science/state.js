d0d1932bc2f26dba26d2329e7ce6077d7baa4da3
ricardoboss <ricardoboss@users.noreply.github.com>
2022-11-02 21:54:51 +0000

    deploy: 3c930e5f88c061d92e3063a25ebe776fbceac167

--- _nuxt/static/1666434483/blog/2020-12-23-its-rocket-science/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666434483",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2020-12-23-its-rocket-science",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"57f4ff29"}}}}(null));
