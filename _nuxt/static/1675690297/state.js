68bc1f672c08e87206efe6a89cfe4f603d96bf78
ricardoboss <ricardoboss@users.noreply.github.com>
2023-02-06 13:31:45 +0000

    deploy: 4ebc01bfdca7d986bdbb7943f52f11d660ef427a

---
+++ _nuxt/static/1675690297/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675690297",layout:"default",error:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5b93e56e"}}}}(null,"\u002F"));
