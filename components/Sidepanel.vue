0447310b6c053681e1af43747b729194ed658e1b
Ricardo Boss <contact@ricardoboss.de>
2023-11-30 23:58:16 +0100

    Formatting

--- components/Sidepanel.vue
+++ components/Sidepanel.vue
@@ -1,5 +1,5 @@
 <template>
-	<aside class="sticky-top align-self-start">
+  <aside class="sticky-top align-self-start">
     <img src="/img/ricardo-boss-2023.square.min.png" alt="Ricardo Boss 2020" style="border-width: 2px !important;" class="border rounded-circle border-primary shadow-lg w-75 mb-4">
 
     <span class="h3 font-weight-light">Ricardo Boss</span>
@@ -11,22 +11,23 @@
 </template>
 
 <script>
-	import SloganRoller from "./SloganRoller.vue";
-  export default {
-		name: "Sidepanel",
-    components: {SloganRoller},
+import SloganRoller from "./SloganRoller.vue";
 
-    data() {
-		  return {
-		    slogans: [
-		      'Software Developer',
-          'Fullstack Developer',
-          '.NET Developer',
-          'PHP Developer',
-          'Backend Developer',
-          'Webdeveloper',
-        ],
-      }
-    }
+export default {
+  name: "Sidepanel",
+  components: { SloganRoller },
+
+  data() {
+    return {
+      slogans: [
+        "Software Developer",
+        "Fullstack Developer",
+        ".NET Developer",
+        "PHP Developer",
+        "Backend Developer",
+        "Webdeveloper"
+      ]
+    };
   }
+};
 </script>
