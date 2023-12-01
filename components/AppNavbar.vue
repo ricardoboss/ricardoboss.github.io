5564a6dea0a6fea36a5d2f6955ad29e3c0016953
Ricardo Boss <contact@ricardoboss.de>
2023-12-01 20:46:49 +0100

    Added basic default layout

---
+++ components/AppNavbar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav>Nav</nav>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import AppSidepanel from '~/components/AppSidepanel.vue'
+import AppNavbar from '~/components/AppNavbar.vue'
+import AppFooter from '~/components/AppFooter.vue'
+</script>
+
+<template>
+  <div id="default-layout">
+    <app-sidepanel id="sidepanel" />
+    <app-navbar id="navbar" />
+    <slot id="content" />
+    <app-footer id="footer" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#default-layout {
+  display: grid;
+  grid-template:
+    'sidepanel navbar ' auto
+    'sidepanel content' minmax(100dvh, 1fr)
+    'sidepanel footer' auto / auto 1fr;
+}
+
+#sidepanel {
+  grid-area: sidepanel;
+}
+
+#navbar {
+  grid-area: navbar;
+}
+
+#footer {
+  grid-area: footer;
+}
+</style>
