e23c20fd45ed75296d3c1bb1a5b027e078a958b9
Ricardo Boss <contact@ricardoboss.de>
2023-12-02 20:40:16 +0100

    Implemented simple navbar

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav>Nav</nav>
+  <nav>
+    <router-link to="/blog">Blog</router-link>
+    <router-link to="/curriculum-vitae">Curriculum Vitae</router-link>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/portfolio">Portfolio</router-link>
+    <router-link to="/contact">Contact</router-link>
+  </nav>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '@/style/global';
+
+nav {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 5dvh;
+  margin-top: 0.5rem;
+  margin-right: 0.5rem;
+
+  & > * {
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    border-radius: 0.5rem;
+
+    &.router-link-active {
+      background: $accent;
+      color: $body-bg;
+    }
+  }
+}
+</style>
