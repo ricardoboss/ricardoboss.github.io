7e42b017ea939839870f58d30b36537dc21694a3
Ricardo Boss <contact@ricardoboss.de>
2023-12-03 04:09:46 +0100

    Added some basic footer content

--- components/structural/AppFooter.vue
+++ components/structural/AppFooter.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer>Footer</footer>
+  <footer>
+    <p>&copy; 2023 Ricardo Boss. All rights reserved.</p>
+  </footer>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 5dvh;
+  margin: 4em 0;
+
+  opacity: 0.5;
+}
+</style>
